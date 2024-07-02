6509d8dc82615499c9aa395571ad4dc78b1a691c
wangzhenhuiOne <1664002766@qq.com>
2024-07-02 15:54:15 +0800

    Signed-off-by: wangzhenhuiOne <1664002766@qq.com>
    Signed-off-by: wangzhenhuiOne <1664002766@qq.com>
    
    Signed-off-by: wangzhenhuiOne <1664002766@qq.com>
    
    适配ComponentContent<T>泛型写法
    
    Signed-off-by: wangzhenhuiOne <1664002766@qq.com>

--- automock/mock-generate/src/generate/generateClassDeclaration.ts
+++ automock/mock-generate/src/generate/generateClassDeclaration.ts
@@ -282,7 +282,7 @@ function addCustomeClass(
   const regex = new RegExp(`\\sclass\\s*${className}\\s*(<|{|extends|implements)`);
   const results = fileContent.match(regex);
   if (!results && !hasImportType) {
-    mockClassBody = `class ${className} {};\n`;
+    mockClassBody = `export class ${className} {};\n`;
   }
   return mockClassBody;
 }
