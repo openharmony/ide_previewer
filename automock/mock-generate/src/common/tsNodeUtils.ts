80940e5f9f440e6a36ad6f58239b24aceca2f0a1
wangzhenhuiOne <1664002766@qq.com>
2025-07-15 09:45:10 +0800

    处理type类型含有arkts1.2标签的接口
    Signed-off-by: wangzhenhuiOne <1664002766@qq.com>

--- automock/mock-generate/src/common/tsNodeUtils.ts
+++ automock/mock-generate/src/common/tsNodeUtils.ts
@@ -874,6 +874,9 @@ function handleModuleBody(
   switch (node.kind) {
     case SyntaxKind.ModuleBlock: {
       node.statements.forEach(statement => {
+        if (!isArktsOne(statement, sourceFile)) {
+          return;
+        }
         handleStatement(sourceFile, statement, mockBuffer, members, parent);
       });
       return;
